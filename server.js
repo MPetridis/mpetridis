24a33e06acf3c72be9f12557a1da9f825fa47082
Petridis Minas <m.petridis2002@gmail.com>
2022-12-25 11:52:05 +0200

    ejs

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 app.listen(3000);
