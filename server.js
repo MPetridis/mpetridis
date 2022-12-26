fc80ca31806eaac939de81d1ad2d31104354a4ce
Petridis Minas <m.petridis2002@gmail.com>
2022-12-26 11:54:08 +0200

    layout

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+var ejsLayouts = require("express-ejs-layouts");
 const app = express();
 
 app.use(express.static("public"));
+app.set("views", "./views");
 app.set("view engine", "ejs");
+app.use(ejsLayouts);
+
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("hello");
 });
 
 app.listen(3000);
